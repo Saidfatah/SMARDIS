bcb42b0841920a331b1911f079bfe4e33d362eda
SaidFatah <said_designer@outlook.com>
2021-01-22 17:37:52 +0100

    fixed clents ordering component

---
+++ src/components/AdminPanel/Schedule/ClientsOrdering.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+
+const ClientsOrdering=()=> {
+    return (
+        <View>
+            <Tetx>clients ordering</Tetx>
+        </View>
+    )
+}
+
+export default ClientsOrdering
